30e93fc553766add71a6a47b036528f48f00ed2e
dsad2221s <ghksgml0555@naver.com>
2023-01-19 21:02:58 +0900

    서블릿을 통해서 요청, 응답해보기
    서블릿을 통해서 요청, 응답해보기

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,16 +2,16 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan /* 스프링 부트에서 서블릿을 쓰려면 이 애노테이션을 붙혀주면
+                      스프링이 자동으로 현재 내 패키지를 포함해서 하위패키지를 다 뒤져서
+                      서블릿을 다 찾아서 자동으로 서블릿을 등록해서 실행할 수 있도록 해준다.
+                       ==> 서블릿 자동등록 */
 @SpringBootApplication
 public class ServletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-	//test
-	//test2
-	//test3
-	//test4
-	//test5
 }
---
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+//이름을주고 , /hello로 오면 이게 실행된다.
+public class HelloServlet extends HttpServlet {//서블릿은 HttpServlet을 상속받음
+
+    //컨트롤 + O로 service 찾아서 좌물쇠 잠긴게 protected
+    //이렇게 해두면 서블릿이 호출되면 서비스 메소드가 호출된다.
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        //서블릿은 쿼리파라미터를 getParameter로 편하게 읽을 수 있게 해준다.
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain"); //단순문자를 보낼것
+        response.setCharacterEncoding("utf-8");//문자세트 인코딩 정보 > 요즘은 utf-8
+        response.getWriter().write("hello " + username); //http메세지 바디에 데이터가 들어간다.
+    }
+}
