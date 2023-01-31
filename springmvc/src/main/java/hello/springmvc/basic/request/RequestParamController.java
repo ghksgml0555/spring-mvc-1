fdad1576d2fd56f7d154bd05ba8dd19267364bd6
dsad2221s <ghksgml0555@naver.com>
2023-01-31 17:34:38 +0900

    스프링으로 요청파라미터를 조회하는방법을 단계적으로 조회
    스프링으로 요청파라미터를 조회하는방법을 단계적으로 조회

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,12 +1,17 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,5 +24,71 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody //반환값을 http 응답메세지에 넣어서 반환한다. > @RestController처럼
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge){
+
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    @ResponseBody //반환값을 http 응답메세지에 넣어서 반환한다. > @RestController처럼
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username, //변수명과 파라미터명이 같을때 v2에 비해 이렇게 생략가능
+            @RequestParam int age){
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody //반환값을 http 응답메세지에 넣어서 반환한다. > @RestController처럼
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age){//요청파라미터명과 변수명이 같으면 @RequestParam조차 생략가능
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody //반환값을 http 응답메세지에 넣어서 반환한다. > @RestController처럼
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) int age){
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody //반환값을 http 응답메세지에 넣어서 반환한다. > @RestController처럼
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age){
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody //반환값을 http 응답메세지에 넣어서 반환한다. > @RestController처럼
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap){
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData){
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData){
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
     }
 }
